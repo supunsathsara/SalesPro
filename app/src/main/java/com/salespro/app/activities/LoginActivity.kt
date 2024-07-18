721691b96f6b18a5ef7c8c04bf56a66f10d32aaa
Savindu Supun <contact@supunsathsara.com>
2024-07-19 05:20:29 +0530

    Impl login and reset password

--- app/src/main/java/com/salespro/app/activities/LoginActivity.kt
+++ app/src/main/java/com/salespro/app/activities/LoginActivity.kt
@@ -2,47 +2,127 @@ package com.salespro.app.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
+import com.google.android.material.snackbar.Snackbar
 import com.salespro.app.R
+import com.salespro.app.databinding.ActivityLoginBinding
 
+import com.salespro.app.util.Resource
+import com.salespro.app.util.setupBottomSheetDialog
+import com.salespro.app.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
+
+    private lateinit var binding: ActivityLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        // Inflate the layout using View Binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Apply window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_main_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val register = findViewById<TextView>(R.id.tv_dont_have_an_account)
-        register.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-            finish();
+        // Initialize the views
+        val emailEditText = findViewById<EditText>(R.id.ed_email_login)
+        val passwordEditText = findViewById<EditText>(R.id.ed_password_login)
+        val loginButton = findViewById<CircularProgressButton>(R.id.btn_login)
+        val registerTextView = findViewById<TextView>(R.id.tv_dont_have_an_account)
+
+        binding.apply {
+            btnLogin.setOnClickListener {
+                val email = edEmailLogin.text.toString()
+                val password = edPasswordLogin.text.toString()
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.login(email, password)
+            }
         }
 
-        //! CODE FOR LOGIN BTN LISTENER
-        val emailEditText: EditText = findViewById(R.id.ed_email_login)
-        val passwordEditText: EditText = findViewById(R.id.ed_password_login)
-        val loginButton: CircularProgressButton = findViewById(R.id.btn_login_fragment)
+        // Set the click listener for the register text view
+        registerTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
-        // Set an OnClickListener on the login button
-        loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+        binding.tvForgotPassword.setOnClickListener {
+           setupBottomSheetDialog {  email ->
+               viewModel.resetPassword(email)
 
-            // Show the email and password as a toast message
-            Toast.makeText(this, "Email: $email\nPassword: $password", Toast.LENGTH_LONG).show()
+           }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.resetPassword.collect { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        binding.btnLogin.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.btnLogin.revertAnimation()
+                       Snackbar.make(binding.root, "Reset password email sent", Snackbar.LENGTH_SHORT).show()
+                    }
+                    is Resource.Error -> {
+                        binding.btnLogin.revertAnimation()
+                        resource.message?.let { Log.e(TAG, it) }
+                        Toast.makeText(this@LoginActivity, resource.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        // Observe the login flow
+        lifecycleScope.launchWhenStarted {
+            viewModel.login.collect { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        binding.btnLogin.startAnimation()
+                    }
+                    is Resource.Success -> {
+                       binding.btnLogin.revertAnimation()
+
+                        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    is Resource.Error -> {
+                        binding.btnLogin.revertAnimation()
+                        resource.message?.let { Log.e(TAG, it) }
+
+
+                        Toast.makeText(this@LoginActivity, resource.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
         }
     }
 
