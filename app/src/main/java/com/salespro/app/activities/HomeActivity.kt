59968994b56ae5557e34791f43d9e66210b278e2
shawindu madushan <106295341+maduzan@users.noreply.github.com>
2024-07-19 17:08:39 +0530

    add bottm_nav_backgrounds

--- app/src/main/java/com/salespro/app/activities/HomeActivity.kt
+++ app/src/main/java/com/salespro/app/activities/HomeActivity.kt
@@ -5,17 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.salespro.app.R
+import com.salespro.app.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navController = findNavController(R.id.host_fragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
