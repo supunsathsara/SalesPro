8fd607c615a02b53547b43dabfbd31d8a4ef839d
Savindu Supun <contact@supunsathsara.com>
2024-07-20 01:55:35 +0530

    fix: impl close button on product details fragment

--- app/src/main/java/com/salespro/app/fragments/ProductDetailsFragment.kt
+++ app/src/main/java/com/salespro/app/fragments/ProductDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.salespro.app.R
@@ -32,6 +33,10 @@ class ProductDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val product = args.product
+
+        binding.imgClose.setOnClickListener {
+            findNavController().navigateUp()
+        }
 
         binding.apply {
             tvProductName.text = product.name
